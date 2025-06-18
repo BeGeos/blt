97d73774cdc019bbd720d05a74b5c4896ebbcacd
BeGeos <begeosdev@gmail.com>
2025-06-18 22:30:30 +0200

    chore: move middlewares to setup

--- go-echo/cmd/apps/server.go
+++ go-echo/cmd/apps/server.go
@@ -6,28 +6,15 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"golang.org/x/time/rate"
 
 	"github.com/BeGeos/go-echo/internal"
 )
 
 func main() {
 	e := echo.New()
-	Env := os.Getenv("APP_ENV")
 
 	// run setup based on environment
-	internal.Setup(Env, e)
-
-	e.Use(middleware.RequestID())
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.BodyLimit("10M"))
-	e.Use(middleware.CORS())
-	e.Use(middleware.Gzip())
-	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(rate.Limit(30)))) // 30 requests per second
-	e.Use(middleware.Secure())
-	e.Use(middleware.Timeout())
+	internal.Setup(e)
 
 	e.GET("/", func(c echo.Context) error {
 		luyckyNumber := os.Getenv("LUCKY_NUMBER")
--- go-echo/internal/setup.go
+++ go-echo/internal/setup.go
@@ -8,9 +8,11 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/time/rate"
 
 	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func loadEnv(env string) error {
@@ -21,19 +23,10 @@ func loadEnv(env string) error {
 	return nil
 }
 
-func Setup(env string, e *echo.Echo) error {
-	fmt.Println("Setting up...")
-
-	switch env {
-	case "dev":
-		setupDev(e)
-	case "prod":
-		setupProd()
-	case "staging":
-		setupStaging()
-	}
-
-	return errors.New("APP_ENV is not set or invalid")
+func checkEnvVariabled() error {
+	// these are the variables that must be set in the environment
+	Vars := []string{}
+	return nil
 }
 
 func setupDev(e *echo.Echo) {
@@ -72,4 +65,36 @@ func setupStaging() {
 		log.Println("❌ Failed to load env: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func Setup(e *echo.Echo) error {
+	Env := os.Getenv("APP_ENV")
+	fmt.Println("Setting up...")
+
+	switch Env {
+	case "dev":
+		setupDev(e)
+	case "prod":
+		setupProd()
+	case "staging":
+		setupStaging()
+	default:
+		return errors.New("APP_ENV is not set or invalid")
+	}
+
+	if err := checkEnvVariabled(); err != nil {
+		log.Println("❌ Failed to check env variables: %v\n", err)
+	}
+
+	e.Use(middleware.RequestID())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.BodyLimit("10M"))
+	e.Use(middleware.CORS())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(rate.Limit(30)))) // 30 requests per second
+	e.Use(middleware.Secure())
+	e.Use(middleware.Timeout())
+
+	return nil
 }

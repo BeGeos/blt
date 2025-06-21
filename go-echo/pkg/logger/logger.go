020d196e932603d159e052332132bd55b937e28a
BeGeos <begeosdev@gmail.com>
2025-06-21 23:06:32 +0200

    chore: refactor logger

--- go-echo/pkg/logger/logger.go
+++ go-echo/pkg/logger/logger.go
@@ -7,22 +7,34 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/BeGeos/go-echo/internal/utils"
 )
 
-var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+func getLoggerLevel() slog.Level {
+	Env := utils.GetEnv()
+	if Env == "dev" {
+		return slog.LevelDebug
+	}
+
+	return slog.LevelInfo // Default to Info level for production and staging
+}
+
+var (
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	Logger = slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: getLoggerLevel()}))
+)
 
 func LogValuesFunc(c echo.Context, v middleware.RequestLoggerValues) error {
+	request := slog.Group("request", slog.String("uri", v.URI), slog.String("method", v.Method), slog.Int("status", v.Status))
+
 	if v.Error == nil {
 		logger.LogAttrs(context.Background(), slog.LevelInfo, "REQUEST",
-			slog.String("uri", v.URI),
-			slog.String("method", v.Method),
-			slog.Int("status", v.Status),
+			request,
 		)
 	} else {
 		logger.LogAttrs(context.Background(), slog.LevelError, "REQUEST_ERROR",
-			slog.String("uri", v.URI),
-			slog.String("method", v.Method),
-			slog.Int("status", v.Status),
+			request,
 			slog.String("err", v.Error.Error()),
 		)
 	}

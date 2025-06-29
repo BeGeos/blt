a830f99f63f67c79f56e6a7e50f34e6a5be19477
BeGeos <begeosdev@gmail.com>
2025-06-29 18:04:59 +0200

    chore: set settings for jwt

--- go-echo/internal/settings/main.go
+++ go-echo/internal/settings/main.go
@@ -1,5 +1,26 @@
 package settings
 
+import (
+	"os"
+)
+
+func getEnvKey(key, defaultValue string) string {
+	err := LoadEnv()
+	if err != nil {
+		return defaultValue
+	}
+
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+
+	return defaultValue
+}
+
 const AppEnvKey = "APP_ENV"
 
 var AllowedEnvs = []string{"dev", "prod", "staging"}
+
+// Authentication settings
+
+var JwtSecretKey = getEnvKey("JWT_SECRET_KEY", "")

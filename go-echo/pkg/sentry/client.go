8bf99f016488a8f1bfecfbf9507a294046dc3058
BeGeos <begeosdev@gmail.com>
2025-07-04 22:07:43 +0200

    feat: add sentry

---
+++ go-echo/pkg/sentry/client.go
@@ -0,0 +1,45 @@
+package sentry
+
+import (
+	"github.com/BeGeos/go-echo/internal/settings"
+	"github.com/getsentry/sentry-go"
+)
+
+type Client struct {
+	Options sentry.ClientOptions
+}
+
+func GetSentryClientOptions(env string) sentry.ClientOptions {
+	switch env {
+	case settings.EnvStaging:
+		return getStagingClientOptions()
+	case settings.EnvProduction:
+		return getProductionClientOptions()
+	}
+
+	return sentry.ClientOptions{}
+}
+
+func getProductionClientOptions() sentry.ClientOptions {
+	return sentry.ClientOptions{
+		Dsn:              settings.SentryDsn,
+		Debug:            true,
+		AttachStacktrace: true,
+		TracesSampleRate: 0.3,
+		Environment:      settings.EnvProduction,
+	}
+}
+
+func getStagingClientOptions() sentry.ClientOptions {
+	return sentry.ClientOptions{
+		Dsn:              settings.SentryDsn,
+		Debug:            true,
+		AttachStacktrace: true,
+		TracesSampleRate: 0.8,
+		Environment:      settings.EnvStaging,
+	}
+}
+
+func (c *Client) Init() error {
+	return sentry.Init(c.Options)
+}

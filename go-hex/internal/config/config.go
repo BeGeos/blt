2081e784fa4297f4dc12b2361c435fcbc31238e2
BeGeos <begeosdev@gmail.com>
2025-07-06 23:05:16 +0200

    chore: run config once

--- go-hex/internal/config/config.go
+++ go-hex/internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 )
@@ -20,21 +21,30 @@ type DatabaseConfig struct {
 	URL string
 }
 
+var (
+	cfg  *Config
+	once sync.Once
+)
+
 func Load() (*Config, error) {
-	env := getEnv("GO_ENV", "dev")
+	once.Do(func() {
+		env := getEnv("GO_ENV", "dev")
 
-	_ = godotenv.Load(".env")        // always try base
-	_ = godotenv.Load(".env." + env) // e.g., .env.local or .env.dev
+		_ = godotenv.Load(".env")        // always try base
+		_ = godotenv.Load(".env." + env) // e.g., .env.local or .env.dev
 
-	return &Config{
-		Env: env,
-		Server: ServerConfig{
-			Port: getEnv("SERVER_PORT", ":8080"),
-		},
-		Database: DatabaseConfig{
-			URL: getEnv("DATABASE_URL", ""),
-		},
-	}, nil
+		cfg = &Config{
+			Env: env,
+			Server: ServerConfig{
+				Port: getEnv("SERVER_PORT", ":8080"),
+			},
+			Database: DatabaseConfig{
+				URL: getEnv("DATABASE_URL", ""),
+			},
+		}
+	})
+
+	return cfg, nil
 }
 
 func getEnv(key, fallback string) string {
